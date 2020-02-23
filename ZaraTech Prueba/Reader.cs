018c282dd325bd434a0b2f254e87370bae896b91
RaulRuizVela <ruizvela271@gmail.com>
2020-02-23 18:07:42 +0100

    funcion que guarda el excel en arrays divididos de fecha, cierre y apertura

---
+++ ZaraTech Prueba/Reader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ZaraTech_Prueba
+{
+    public class Reader
+    {
+            List<string> dates = new List<string>();
+            List<string> openings = new List<string>();
+            List<string> closures = new List<string>();
+        void Read()
+        {
+            string path = @"c:\Users\usuario\source\repos\ZaraTech Prueba\stocks-ITX.csv";
+            string[] lines = File.ReadAllLines(path);
+            foreach (string line in lines)
+            {
+
+                string[] columns = line.Split(';');
+                dates.Add(columns[0]);
+                openings.Add(columns[1]);
+                closures.Add(columns[2]);
+            }
+        }
+    }
+}
---
+++ ZaraTech PruebaTests/ReaderTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ZaraTech_Prueba;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZaraTech_Prueba.Tests
+{
+    [TestClass()]
+    public class ReaderTests
+    {
+        [TestMethod()]
+        public void ReadDatesTest()
+        {
+            Reader reader = new Reader();
+            string line = reader.ReadDates();
+            Assert.IsTrue(line == "28-dic-2017");
+        }
+    }
+}
